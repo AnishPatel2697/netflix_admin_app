d0788bb1d9301578c98a3c76fc2d3d8c107fd00b
Anish Patel <USCS-XXX@scs.com>
2021-11-30 12:46:00 -0800

    Netflix_Admin_App_functional

---
+++ src/context/movieContext/apiCalls.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+import {
+  createMovieFailure,
+  createMovieStart,
+  createMovieSuccess,
+  deleteMovieFailure,
+  deleteMovieStart,
+  deleteMovieSuccess,
+  getMoviesFailure,
+  getMoviesStart,
+  getMoviesSuccess,
+} from "./MovieActions";
+
+export const getMovies = async (dispatch) => {
+  dispatch(getMoviesStart());
+  try {
+    const res = await axios.get("/movies", {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    dispatch(getMoviesSuccess(res.data));
+  } catch (err) {
+    dispatch(getMoviesFailure());
+  }
+};
+
+//create
+export const createMovie = async (movie, dispatch) => {
+  dispatch(createMovieStart());
+  try {
+    const res = await axios.post(
+      "https://netflix-project-server.herokuapp.com/api/movies",
+      movie,
+      {
+        headers: {
+          token:
+            "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+        },
+      }
+    );
+    console.log("Movie Creation Result: ", res);
+    if(res.status == 201){
+      dispatch(createMovieSuccess(res.data));
+      return true;
+    }
+    
+  } catch (err) {
+    dispatch(createMovieFailure());
+  }
+};
+
+//delete
+export const deleteMovie = async (id, dispatch) => {
+  dispatch(deleteMovieStart());
+  try {
+    await axios.delete("/movies/" + id, {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    dispatch(deleteMovieSuccess(id));
+  } catch (err) {
+    dispatch(deleteMovieFailure());
+  }
+};
